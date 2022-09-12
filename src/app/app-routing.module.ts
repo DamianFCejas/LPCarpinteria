a36a5b253cbd8115cb14d01592466fb708b8c193
Damian Cejas <damianfcejas@gmail.com>
2022-09-11 23:28:43 -0300

    rutas creadas, contacto creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'nosotros',component: NosotrosComponent},
+  {path:'contacto',component: ContactoComponent},
+  {path:'productos',component: ProductosComponent},
+  {path:'inicio',component: InicioComponent},
+  {path:'**',redirectTo:'inicio',pathMatch:'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { ProductosComponent } from './components/productos/productos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     NosotrosComponent,
-    ProductosComponent
+    ProductosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
