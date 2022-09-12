ae5dd518c58cf0534baf543f6366e54e2411179e
Damian Cejas <damianfcejas@gmail.com>
2022-09-11 23:13:47 -0300

    componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
